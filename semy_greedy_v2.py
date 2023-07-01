6340cc23ee82a5430c17431e86216507468bf8d7
Max Natanael Starke <maxnatanaelstk@gmail.com>
2023-07-01 15:59:59 -0300

    criando metodos genericos

--- semy_greedy_v2.py
+++ semy_greedy_v2.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from utils import calculate_route_cost, get_distance_matrix, load_vrp_instance
 from vehicle_routing_problem import VehicleRoutingProblem
+import random
 
 
 # TODO garantir que todos caminhoes sejam usados. preencher todos caminhoões simultaneamente ou com alguma outra estratégia
@@ -10,15 +11,12 @@ class SemiGreedyCRVP2(VehicleRoutingProblem):
 
     """ Construtiva semi gulosa, preenche um caminhão de cada vez, 
     melhorar para verificar em qual caminhão deve ser posto """
+
+    k_percentage = None
     k = None
 
-    def get_sorted_customers(self, available_customers, current_customer):
-        candidate_customers = []
-        for customer in available_customers:
-            distance = self.dist_matrix[current_customer-1][customer-1]
-            candidate_customers.append((customer, distance))
-        candidate_customers.sort(key=lambda x: x[1])
-        candidate_customers = candidate_customers[:self.k]
+    def get_k_based_customers(self, current_customer, available_customers):
+        candidate_customers = self.get_closest_customers(current_customer,available_customers)[:self.k]
         return list(map(lambda x: x[0], candidate_customers))
     
     def get_next_customer(self, customers, capacity):
@@ -30,10 +28,14 @@ class SemiGreedyCRVP2(VehicleRoutingProblem):
             customers.remove(chosen_customer)
         return next_customer
     
+    def set_k_parameter(self,customers_amount):
+        self.k = int(customers_amount * (self.k_percentage / 100))
+        self.k = 1 if not self.k else self.k
 
     def run(self, max_iterations, k_percentage):
+        self.k_percentage = k_percentage
 
-        # armazena o melhor conjunto de rotas e melhores custos até então
+        # armazena o melhor conjunto de rotas e custos 
         self.best_routes = []
         self.best_cost = None       
 
@@ -46,27 +48,32 @@ class SemiGreedyCRVP2(VehicleRoutingProblem):
             available_customers = list(range(2, len(self.demands)+1))
             visited_customers = []
 
-            while available_customers:
-               
+            current_route = routes[0]
+            while available_customers:        
                 max_iterations -= 1
 
-                # seleciona a rota baseado naquela que contem o menor numero de clientes
-                current_route = min(routes, key=len)           
 
                 # seleciona os k melhores elementos a serem sorteados
-                customers_amount = len(available_customers)-1
-                self.k = int(customers_amount * (k_percentage / 100))
-                self.k = 1 if not self.k else self.k
-                # seleciona os mais próximos baseados prametro k
-                candidate_customers = self.get_sorted_customers(available_customers,current_route[-1])
+                self.set_k_parameter(len(available_customers)-1)
+
+                # seleciona os mais próximos baseados parametro k
+                candidate_customers = self.get_k_based_customers(current_route[-1], available_customers)
 
                 # sorteia um deles
-                next_customer = self.get_next_customer(candidate_customers, self.vehicle_capacity)
+                next_customer = self.get_next_customer(candidate_customers, self.get_remaining_capacity(current_route))
+
+                # caso não haja proximo cliente disponivel para rota, erscolhemos outra aleatoriamente para utilizar
+                if not next_customer:
+                    current_route = routes[random.randint(0, (self.num_vehicles)-1)]
+                    continue
 
                 # adiciona o cliente na rota atual
                 current_route.append(next_customer)
                 available_customers.remove(next_customer)
                 visited_customers.append(next_customer)
+
+                # seleciona a rota baseado naquela que contem o menor numero de clientes
+                current_route = min(routes, key=len)           
             
             for route in routes:
                 route.append(1) 
--- vehicle_routing_problem.py
+++ vehicle_routing_problem.py
@@ -6,6 +6,17 @@ class VehicleRoutingProblem:
         self.file_path = file_path
         self.vehicle_capacity, self.num_vehicles, self.optimal_value, self.dimension, self.node_coords, self.demands = load_vrp_instance(file_path)
         self.dist_matrix = get_distance_matrix(self.dimension, self.node_coords)
+    
+    def get_closest_customers(self, current_customer, available_customers):
+        closest_cutomers = []
+        for customer in available_customers:
+            distance = self.dist_matrix[current_customer-1][customer-1]
+            closest_cutomers.append((customer, distance))
+        closest_cutomers.sort(key=lambda x: x[1])
+        return closest_cutomers
+
+    def get_remaining_capacity(self, route):
+        return self.vehicle_capacity - sum(self.demands[node] for node in route)
 
     def run(self):
         raise NotImplementedError()
