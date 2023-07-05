1927410774745a7c8687997ff84f692506596130
JEKrieger <joaoeduardokrieger123@gmail.com>
2023-07-05 20:23:25 -0300

    main testes

--- main.py
+++ main.py
@@ -1,41 +1,73 @@
-from old_heuristics.iterated_greedy_old import IteratedGreedyCRVP
-from old_heuristics.iterated_semi_greedy import SemiGreedyCRVP
 import statistics
+from iterated_greedy import IteratedGreedyCRVP
 
+from semi_greedy import SemiGreedyCRVP
 from simple_local_search import SimpleLocalSearch
+
+#from simple_local_search import SimpleLocalSearch
 
 # from iterated_greedy import IteratedGreedyCRVP
 
 instances = [
     "instances/A/A-n32-k5.vrp",
+    "instances/A/A-n33-k6.vrp",
+    "instances/A/A-n37-k5.vrp",
+    "instances/A/A-n39-k5.vrp",
+    "instances/A/A-n44-k6.vrp",
     "instances/A/A-n45-k7.vrp",
+    "instances/A/A-n46-k7.vrp",
+    "instances/A/A-n48-k7.vrp",
+    "instances/A/A-n53-k7.vrp",
+    "instances/A/A-n54-k7.vrp",
+    "instances/A/A-n60-k9.vrp",
+    "instances/A/A-n62-k8.vrp",
     "instances/A/A-n63-k10.vrp",
+    "instances/A/A-n63-k9.vrp",
     "instances/A/A-n65-k9.vrp",
-    "instances/A/A-n80-k10.vrp",
-    "instances/Uchoa_2014/X/X-n101-k25.vrp",
-    "instances/Uchoa_2014/X/X-n110-k13.vrp",
-    "instances/Uchoa_2014/X/X-n125-k30.vrp",
-    "instances/Uchoa_2014/X/X-n139-k10.vrp",
-    "instances/Uchoa_2014/X/X-n148-k46.vrp",
-    "instances/Uchoa_2014/X/X-n153-k22.vrp",
-    "instances/Uchoa_2014/X/X-n167-k10.vrp",
-    "instances/Uchoa_2014/X/X-n176-k26.vrp",
-    "instances/Uchoa_2014/X/X-n186-k15.vrp",
-    "instances/Uchoa_2014/X/X-n195-k51.vrp",
-    "instances/Uchoa_2014/X/X-n251-k28.vrp",
-    "instances/Uchoa_2014/X/X-n284-k15.vrp",
-    "instances/Uchoa_2014/X/X-n294-k50.vrp",
+    "instances/A/A-n80-k10.vrp"
 ]
 
-# semy_greedy_results = []
-# for instance in instances:
-#     semi_greedy = SemiGreedyCRVP(file_path=instance)
-#     results = semi_greedy.run(max_iterations=500, k_percentage=50)
-#     semy_greedy_results.append(results[2]/results[1])
-# print(sum(semy_greedy_results) / len(semy_greedy_results))
+'''
+semy_greedy_results = []
+for instance in instances:
+    semi_greedy = SemiGreedyCRVP(file_path=instance,k_percentage=15)
+    results = semi_greedy.run()
+    
+    print(instance," :",results["optimal_cost"]," - ",results["solution_cost"]," - ",results["optimal_cost"]/results["solution_cost"])
+    
+    semy_greedy_results.append(results["optimal_cost"]/results["solution_cost"])
 
-# iterated_greedy = IteratedGreedyCRVP(file_path="instances\A\A-n32-k5.vrp")
-# print(iterated_greedy.run(max_iterations=5000, destruction_percentage=20))
+print(sum(semy_greedy_results) / len(semy_greedy_results))
+'''
 
-simple_local_search = SimpleLocalSearch('instances/A/A-n80-k10.vrp')
-print(simple_local_search.run(5000))
+'''
+iterated_greedy_results = []
+for instance in instances:
+    iterated_greedy = IteratedGreedyCRVP(file_path=instance, max_iterations=5000, destruction_percentage=70, k_percentage=15)
+    results = iterated_greedy.run()
+
+    print(instance," :",results["optimal_cost"]," - ",results["solution_cost"]," - ",results["optimal_cost"]/results["solution_cost"]," - ",results["routes"])
+
+    iterated_greedy_results.append(results["optimal_cost"]/results["solution_cost"])
+
+print(sum(iterated_greedy_results) / len(iterated_greedy_results))   
+'''
+
+simple_local_search_results = []
+for instance in instances:
+    simple_local_search = SimpleLocalSearch(file_path=instance,max_iterations=40)
+    results = simple_local_search.run()
+
+    print(instance," :",results["optimal_cost"]," - ",results["solution_cost"]," - ",results["optimal_cost"]/results["solution_cost"])
+
+    simple_local_search_results.append(results["optimal_cost"]/results["solution_cost"])
+
+print(sum(simple_local_search_results) / len(simple_local_search_results)) 
+
+
+
+#iterated_greedy = IteratedGreedyCRVP(file_path="instances\A\A-n32-k5.vrp")
+#print(iterated_greedy.run(max_iterations=5000, destruction_percentage=20))
+
+#simple_local_search = SimpleLocalSearch('instances/A/A-n80-k10.vrp')
+#print(simple_local_search.run(5000))
